fbd2a9a15f4442ac56a4f0b88255934de20c137e
Bhardwaj Avasarala <avbhardwaj@live.com>
2021-11-21 19:51:45 -0800

    Add nerd font installer and rancher clean up shell scripts

---
+++ fonts/nerd-font-installer.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+# install DroidSansMono Nerd Font --> u can choose another at: https://www.nerdfonts.com/font-downloads
+echo "[-] Download fonts [-]"
+echo "Enter Font Name:"
+
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/MesloNerdFont.zip
+unzip DroidSansMono.zip -d ~/.fonts
+fc-cache -fv
+echo "done!"
