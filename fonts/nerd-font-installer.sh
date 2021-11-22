e2aef1f5e35c7904df32ced66e824e8f75856786
Bhardwaj Avasarala <avbhardwaj@live.com>
2021-11-21 20:14:43 -0800

    update info

--- fonts/nerd-font-installer.sh
+++ fonts/nerd-font-installer.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 # Install Nerd Font --> You can choose another at: https://www.nerdfonts.com/font-downloads
 echo "[-] Download fonts [-]"
+echo "Get font names from https://github.com/ryanoasis/nerd-fonts/releases and navigate to assets for the file name or version"
 read -p "Enter Nerd-Font Name: " fontName
 
 read -p "Enter version[default v2.1.0]: " version
