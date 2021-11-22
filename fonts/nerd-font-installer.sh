cfb9619d6c8a47577c489453630c5ecff3650a91
Bhardwaj Avasarala <avbhardwaj@live.com>
2021-11-21 20:06:09 -0800

    update nerd font installer

--- fonts/nerd-font-installer.sh
+++ fonts/nerd-font-installer.sh
@@ -1,10 +1,14 @@
 #/bin/bash
-# install DroidSansMono Nerd Font --> u can choose another at: https://www.nerdfonts.com/font-downloads
+# Install Nerd Font --> You can choose another at: https://www.nerdfonts.com/font-downloads
 echo "[-] Download fonts [-]"
-echo "Enter Font Name:"
+read -p "Enter Nerd-Font Name: " fontName
 
-echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/MesloNerdFont.zip
-unzip DroidSansMono.zip -d ~/.fonts
-fc-cache -fv
+read -p "Enter version[default v2.1.0]: " version
+version=${version:-v2.1.0}
+
+fontUrl=https://github.com/ryanoasis/nerd-fonts/releases/download/$version/$fontName.zip
+echo "Downloading " $fontUrl
+wget $fontUrl
+unzip $fontName.zip -d ~/.local/share/nerd-fonts/
+# fc-cache -fv
 echo "done!"
